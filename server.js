6271d8b19e4e098617b48228763bb20ac38e1102
Ben8842 <z87e99b44r55a@gmail.com>
2020-12-14 19:54:02 -0500

    detect duplicate code added

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
-const { response } = require("express");
+//postman http://localhost:5000/users
+
+//const { response } = require("express");
 const express = require("express");
+const { getMaxListeners } = require("./models/user");
 const user = require("./models/user");
+const cors = require("cors");
+const corsOptions = {
+  origin: "http://localhost:3000",
+  optionsSuccessStatus: 200,
+};
 
 const app = express();
 app.use(express.json());
+
+app.use(cors(corsOptions));
+
 app.get("/users", (req, res) => {
   console.log("finding");
   user.find(
     {
-      email: "superboat@gmail.com",
+      password: "coat",
     },
     (error, data) => {
       if (error) {
@@ -25,13 +36,36 @@ app.post("/users", (req, res) => {
   console.log(req.body.email);
   const body = req.body;
   const userObject = new user(body);
-  userObject.save(function (error) {
-    if (error) {
-      console.log(error);
-    } else {
-      res.status(200).send("user saved");
+  user.findOne(
+    {
+      email: req.body.email,
+    },
+    (error, data) => {
+      if (error) {
+        console.log(error);
+        return res.status(500).send("Server Error during finding");
+      } else if (data == null) {
+        console.log("null!" + data);
+        userObject.save(function (error) {
+          console.log("done.");
+          console.log(error);
+          if (error) {
+            console.log("issues here:" + error);
+            return res.status(500).send("Server Error while saving");
+          } else {
+            return res.status(201).send("user saved");
+          }
+          return;
+        });
+
+        //res.json(data);
+      } else if (data != null) {
+        console.log("not null!" + data);
+        console.log("duplicate !! (email)");
+        return res.status(400).send("duplicate email record");
+      }
     }
-  });
+  );
 });
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
